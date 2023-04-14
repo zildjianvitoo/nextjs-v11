63c2b036586d8fcb7971dbe17f655631d5166273
ZildjianVito <zildjianvito07@gmail.com>
2023-04-14 19:59:29 +0700

    learn getServerSideProps

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,3 +1,30 @@
-export default function NewsList() {
-  return <>List of News Article</>;
+export default function NewsList({ data }) {
+  return (
+    <>
+      <h1>List of News Article</h1>
+      <ul>
+        {data.map((news) => {
+          return (
+            <li key={news.id} style={{ marginTop: "1rem" }}>
+              <h3>
+                {news.id} {news.title}
+              </h3>
+              <p>Category: {news.category}</p>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+}
+
+export async function getServerSideProps() {
+  const res = await fetch("http://localhost:4000/news");
+  const data = await res.json();
+
+  return {
+    props: {
+      data,
+    },
+  };
 }
