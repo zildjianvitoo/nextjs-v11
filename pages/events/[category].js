a80a6f6ffcdbf8ac18feb257d5192f07877c3807
ZildjianVito <zildjianvito07@gmail.com>
2023-04-20 14:10:48 +0700

    done data fetching + prerendering

--- pages/events/[category].js
+++ pages/events/[category].js
@@ -1,11 +1,1 @@
-import React, { useEffect } from "react";
 
-export default function Event() {
-  useEffect(() => {
-    function fetchEventData() {
-      const res = await fetch;
-    }
-  }, []);
-
-  return <h1>[category]</h1>;
-}
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,8 +1,31 @@
-export default function EventList({ events }) {
+import { useState } from "react";
+import { useRouter } from "next/router";
+
+export default function EventList({ eventList }) {
+  const [events, setEvents] = useState(eventList);
+  const router = useRouter();
+
+  async function fetchEvent(category) {
+    const res = await fetch(
+      `http://localhost:4000/events?category=${category}`
+    );
+    const data = await res.json();
+    setEvents(data);
+    router.push(`/events?category=${category}`, undefined, { shallow: true });
+  }
+
   return (
     <>
       <h1>EventList</h1>
       <ul>
+        <div style={{ margin: "1rem", display: "flex", gap: "0.5rem" }}>
+          <button onClick={() => fetchEvent("sports")}>Sports Events</button>
+          <button onClick={() => fetchEvent("food")}>Food Events</button>
+          <button onClick={() => fetchEvent("technology")}>
+            Technology Events
+          </button>
+        </div>
+
         {events.map((evt) => {
           return (
             <li key={evt.id} style={{ marginTop: "1rem" }}>
@@ -19,13 +42,16 @@ export default function EventList({ events }) {
   );
 }
 
-export async function getServerSideProps() {
-  const res = await fetch("http://localhost:4000/events");
+export async function getServerSideProps(context) {
+  const { query } = context;
+  const { category } = query;
+  const queryString = category ? "category=sports" : "";
+  const res = await fetch(`http://localhost:4000/events`);
   const data = await res.json();
 
   return {
     props: {
-      events: data,
+      eventList: data,
     },
   };
 }
